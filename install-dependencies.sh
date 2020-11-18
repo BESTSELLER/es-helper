b5af8ed2cfb3971c3b3916c9c47258ffc03b2460
Peter Brøndum <peter.brondum@bestseller.com>
2020-11-18 08:03:06 +0100

    upgrades

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,13 +8,13 @@ curl -sL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL}/kub
 tar xz && mv kubeval /usr/local/bin/kubeval
 kubeval --version
 
-CONFTEST=0.19.0
+CONFTEST=0.22.0
 printf "\ndownloading conftest ${CONFTEST}\n"
 curl -sL https://github.com/instrumenta/conftest/releases/download/v${CONFTEST}/conftest_${CONFTEST}_Linux_x86_64.tar.gz | \
 tar xz && mv conftest /usr/local/bin/conftest
 conftest --version
 
-KUBESCORE=1.7.1
+KUBESCORE=1.10.0
 printf "\ndownloading kube-score ${KUBESCORE}\n"
 curl -sL https://github.com/zegl/kube-score/releases/download/v${KUBESCORE}/kube-score_${KUBESCORE}_linux_amd64.tar.gz | \
 tar xz && mv kube-score /usr/local/bin/kube-score
